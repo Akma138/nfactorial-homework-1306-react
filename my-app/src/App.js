1946b295eb1c963f74829c0f15b4059f8f783ac1
Akma <napoli-kz@mail.ru>
2022-06-14 18:43:54 +0200

    middle work

---
+++ my-app/src/App.js
@@ -0,0 +1,36 @@
+import one from './img/1.jpg';
+import two from './img/2.jpg';
+import three from './img/3.jpeg';
+import four from './img/4.jpeg';
+import five from './img/5.jpg';
+import six from './img/6.png';
+
+
+
+function App() {
+  return (
+
+    <div>
+ 
+      <div
+      style={{
+        backgroundColor: "black",
+       
+      }}
+      >
+
+      <img src={six} width="1200" height="40"/></div>
+      <img src={one} width="200" height="200" />
+
+      <img src={two} width="200" height="200"/>
+      <img src={three} width="200" height="200"/>
+      <img src={four} width="200" height="200"/>
+      <img src={five} width="200" height="200"/>
+       <div><h1>Hey lets give all you can</h1></div>
+   
+
+    </div>
+  );
+}
+
+export default App;
